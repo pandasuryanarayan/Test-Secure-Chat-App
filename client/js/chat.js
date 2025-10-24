4873f7f48f0c0fcc907f66864f2a5d5ad10a956c
Suryanarayan Puranchand Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2025-10-25 01:05:41 +0530

    Update SERVER_URL for production deployment

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,6 +1,6 @@
 // Define the server URL
 // const SERVER_URL = 'http://localhost:3000';
-const SERVER_URL = 'https://test-secure-chat-app-1.onrender.com';
+const SERVER_URL = 'https://test-secure-chat-app.onrender.com';
 const API_URL = `${SERVER_URL}/api`;
 const IS_PRODUCTION = SERVER_URL.includes('onrender.com') || !SERVER_URL.includes('localhost');
 
